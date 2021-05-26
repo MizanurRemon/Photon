fbc6914a9f0b9368e778de7b4c4808cc80e91615
Remon <remonmizanur@gmail.com>
2021-05-26 23:46:57 +0600

    26 May 11:46PM

--- app/src/main/java/com/example/photon/View/Fragment/HomeScreen_fragment.java
+++ app/src/main/java/com/example/photon/View/Fragment/HomeScreen_fragment.java
@@ -1,26 +1,50 @@
 package com.example.photon.View.Fragment;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.camerakit.CameraKit;
 import com.camerakit.CameraKitView;
 import com.example.photon.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class HomeScreen_fragment extends Fragment {
+import static android.app.Activity.RESULT_OK;
+
+
+public class HomeScreen_fragment extends Fragment implements View.OnClickListener {
 
     CameraKitView cameraKitView;
+    ImageView captureButton, rotateButton;
+    Boolean cameraFaceState = true;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.home_screen_fragment, container, false);
         cameraKitView = view.findViewById(R.id.camera);
+        captureButton = view.findViewById(R.id.captureButtonID);
+        rotateButton = view.findViewById(R.id.rotateButtonID);
+
+        captureButton.setOnClickListener(this);
+        rotateButton.setOnClickListener(this);
         return view;
     }
 
@@ -53,5 +77,59 @@ public class HomeScreen_fragment extends Fragment {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        galleryAddPic();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.captureButtonID) {
+            dispatchTakePictureIntent();
+
+        } else if (v.getId() == R.id.rotateButtonID) {
+
+            cameraKitView.toggleFacing();
+
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        File storageDir = Environment.getExternalStorageDirectory();
+        File image = File.createTempFile(
+                "Img",  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
     }
 }
